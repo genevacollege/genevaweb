264be02c9aaa7e1fbc852eb8403d47e94478baf7
Caleb Hyatt <hcaleb10000@gmail.com>
2023-01-06 15:08:00 -0500

    Update script.js
    Removed unneeded comments.
    Ensured all functions and loops had brackets for readability.
    Gave variables readable names.

--- program-search/script.js
+++ program-search/script.js
@@ -1,29 +1,24 @@
-// This function only allows the code inside to be triggered when the page is fully loaded.
 $(document).ready(function () {
-  // Fetch the JSON output.
   // fetch("https://www.geneva.edu/academics/programs/json/programs.json")
   fetch("[system-asset]/academics/programs/json/programs?raw[/system-asset]")
     .then((results) => results.json())
     .then((initialData) => {
-      // Putting all objects at the beginning so that they can be used wherever they are needed.
       const interestLegends = {
-        "Arts, Design & Communication": "arts",
+        "Arts, Design & Communication": "communication",
         "Biblical Studies, Philosophy & Ministry": "ministry",
         "Business & Sport Management": "business",
         "Computer Science & Technology": "compsci",
         "Education & Teaching": "education",
         "Nursing, Healthcare & Life Sciences": "health",
         "Political Science, History & Languages": "polisci",
-        "Psychology & Social Services": "psych",
+        "Psychology & Social Services": "psychology",
         "Science, Engineering & Mathematics": "stem",
       };
 
-      // Call each function when the page loads to initialize everything.
       urlToSearch();
       init();
       search();
 
-      // Trigger the search function every time an input is changed.
       $("#filter-keyword").on("keyup", function () {
         if ($("#filter-keyword").val().length !== 1) {
           search();
@@ -38,20 +33,11 @@ $(document).ready(function () {
         search();
       });
 
-      // Functions
-      /*
-        This sets the plate to either show all results (if there are
-        no hashes in the URL) or shows the results for the hashes in the URL.
-      */
       function init() {
-        /*
-          Loops through each entry in the JSON fetched and creates a card
-          for each one, adding in all the necessary data.
-        */
-        for (let i = 0; i < initialData.length; i++)
+        for (let i = 0; i < initialData.length; i++) {
           addResult(initialData[i], initialData);
+        }
 
-        // Creates a region that says "No Results Found" and then fades it out of view.
         $("#results").append(`
           <div class="nothing-found">
             <h1>No Results Found</h1>
@@ -61,45 +47,47 @@ $(document).ready(function () {
       }
 
       function search() {
-        // Initialize empty arrays for data later.
-        var values = [];
+        var keyword, interest, program;
 
-        values.push($("#filter-keyword").val().toLowerCase());
-        values.push($("#filter-interest option:selected").val());
-        values.push($("#filter-program option:selected").val());
+        keyword = $("#filter-keyword").val().toLowerCase();
+        interest = $("#filter-interest option:selected").val();
+        program = $("#filter-program option:selected").val();
 
-        // If the inputs are empty, show everything.
-        if (values[0] == "" && values[1] == "" && values[2] == "")
+        if (keyword == "" && interest == "" && program == "") {
           addResult([], []);
+        }
 
         var results = [];
-        // Looping through each entry in the array grabbed from the JSON.
         for (let i = 0; i < initialData.length; i++) {
-          // Checking is keywords match up.
           var keywordIncluded = false;
-          for (let j = 0; j < initialData[i].keywords.length; j++)
-            if (initialData[i].keywords[j].includes(values[0]))
+          for (let j = 0; j < initialData[i].keywords.length; j++) {
+            if (initialData[i].keywords[j].includes(keyword)) {
               keywordIncluded = true;
+            }
+          }
 
-          // Checking if the search bar and dropdowns match any entries.
           var title = initialData[i].title.toLowerCase();
 
           if (
-            (title.includes(values[0]) || keywordIncluded) &&
-            (initialData[i].interest_types.includes(values[1]) ||
-              values[1] == "") &&
-            (initialData[i].program_info.includes(values[2]) || values[2] == "")
-          )
+            (title.includes(keyword) || keywordIncluded) &&
+            (initialData[i].interest_types.includes(interest) ||
+              interest == "") &&
+            (initialData[i].program_info.includes(program) || program == "")
+          ) {
             results.push(initialData[i]);
+          }
         }
 
-        // Fade in what needs to be faded in and fade out what needs to be faded out.
         $(".card").remove();
-        for (let i = 0; i < results.length; i++) addResult(results[i], results);
+        for (let i = 0; i < results.length; i++) {
+          addResult(results[i], results);
+        }
 
-        // If there's nothing to fade in or fade out, fade in "Nothing Found"
-        if (results.length > 0) $("#results .nothing-found").fadeOut(0);
-        else if (results.length === 0) $("#results .nothing-found").fadeIn(0);
+        if (results.length > 0) {
+          $("#results .nothing-found").fadeOut(0);
+        } else if (results.length === 0) {
+          $("#results .nothing-found").fadeIn(0);
+        }
       }
 
       function addResult(degree, results) {
@@ -113,8 +101,6 @@ $(document).ready(function () {
               .replaceAll(" ", "_")}">
               <div class="card-info">
               <div class="img-holder-landscape">
-              
-              
                 <a href="${degree.link}">
                   <img
                     alt="${degree.title}"
@@ -123,7 +109,6 @@ $(document).ready(function () {
                     loading="lazy"
                     aria-hidden="true"
                   />
-              
               </div>
                   <h2>${degree.title}</h2>
                 <p class="descriptors">${degreeCode()}<span class="card-tag">${degree.program_info.join(
